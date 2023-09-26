524bc839d64463abb2a22be3672726a82beabaf9
Danvick Miller <danvickmiller@live.com>
2023-09-26 22:53:33 +0300

    feat: set up redis for cache and session storage

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,9 @@
 <?php
 
 use yii\caching\FileCache;
+use yii\redis\Cache;
+use yii\redis\Connection;
+use yii\redis\Session;
 
 return [
     'aliases' => [
@@ -9,8 +12,19 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'redis' => [
+            'class' => Connection::class,
+            'hostname' => getenv('REDIS_HOST'),
+            'port' => getenv('REDIS_PORT'),
+            'database' => 0
+        ],
+        'session' => [
+            'class' => Session::class,
+            'keyPrefix' => 'APP_SESSION_'
+        ],
         'cache' => [
-            'class' => FileCache::class,
+            'class' => Cache::class,
+            'keyPrefix' => 'APP_CACHE_',
         ],
     ],
 ];
