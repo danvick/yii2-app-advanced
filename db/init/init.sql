71c47fe399944bf7807c9d27451d068ff3cacdc6
Danvick Miller <danvickmiller@live.com>
2023-09-29 14:25:22 +0300

    feat: improvements to docker: use .env file, added console service to run commands

---
+++ db/init/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS yii2_app_advanced;
+GRANT ALL PRIVILEGES ON yii2_app_advanced.* TO 'root'@'172.18.0.%' IDENTIFIED BY 'root';
+FLUSH PRIVILEGES;
+-- USE yii2_app_advanced;
